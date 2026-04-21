3a157f9b9223ed89518150229a7b46714ece222d
Eric Bwire <biyhem@gmail.com>
2026-04-21 17:05:03 +0300

    OBJECT DETECTION SCRIPT

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2
+from ultralytics import YOLO
+
+def resize(frame, size):
+    width = int(frame.shape[1] * size)
+    height = int(frame.shape[0] * size)
+    dimensions = (width, height)
+    return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+
+def save_image(image):
+    print("Saving image...")
+    cv2.imwrite("output/output_picture.jpg", image)
+    print("Image saved successfully!")
+
+image_path = "images/picture.jpg"
+model_path = "models/yolov8l.pt"
+
+pic = cv2.imread(image_path)
+pic = resize(pic, 0.3)
+if pic is None:
+    raise ValueError("Image not found! Check your path.")
+
+model = YOLO(model_path)
+
+results = model(pic)
+annotated = results[0].plot()
+save_image(annotated)
+
+cv2.imshow("OBJECT DETECTION by ericbleo", annotated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
